7703607beec60f0f2c3b7d1cc3c0214331cc439e
Dawid Uniowski <dawid.uniowski@gmail.com>
2026-07-14 23:06:35 +0200

    added links const

---
+++ src/constants/links.ts
@@ -0,0 +1,21 @@
+export const SOCIAL_LINKS = {
+  linkedin: "https://www.linkedin.com/in/dawid-uniowski",
+  github: "https://github.com/uniowski",
+  portfolio: "https://uniowski.pl",
+  email: "mailto:dawid.uniowski@gmail.com",
+} as const;
+
+export const APPS_LINKS = {
+  tastyDejvit: {
+    deployLink: "https://tastydejvit.netlify.app",
+    trepoLink: "https://github.com/uniowski/tastydejvit",
+  },
+  buvi: {
+    deployLink: "https://buvi.netlify.app",
+    repoLink: "https://github.com/uniowski/buvishop",
+  },
+  alfredWeb: {
+    deployLink: "https://alfred-web.netlify.app",
+    repoLink: "https://github.com/uniowski/alfredweb",
+  },
+} as const;
