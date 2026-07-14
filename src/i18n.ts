ab26cab8546c97a4719cb492419a5a8b84a827ef
Dawid Uniowski <dawid.uniowski@gmail.com>
2026-07-14 23:39:53 +0200

    clean up

--- src/i18n.ts
+++ src/i18n.ts
@@ -6,15 +6,13 @@ import { initReactI18next } from 'react-i18next';
 i18n
   .use(HttpApi).use(LanguageDetector).use(initReactI18next).init({
     fallbackLng: 'en', supportedLngs: ['pl', 'en'], backend: {
-      // Ścieżka do plików JSON (Vite domyślnie serwuje folder public z głównego adresu "/")
       loadPath: '/locales/{{lng}}/{{ns}}.json',
     },
 
     interpolation: {
-      escapeValue: false, // React sam dba o bezpieczeństwo XSS
+      escapeValue: false,
     },
 
-    // Opcjonalne: ułatwia debugowanie w konsoli przeglądarki podczas developmentu
     debug: import.meta.env.DEV,
   });
 
