9cced35f99261e25b0c1ffaf138002afda4461ef
Dawid Uniowski <dawid.uniowski@gmail.com>
2026-07-17 23:22:03 +0200

    refactor: change project title heading from h4 to h3 for consistency

--- src/components/tile/Tile.tsx
+++ src/components/tile/Tile.tsx
@@ -10,7 +10,7 @@ function Tile({ item }: TileProps) {
   return (
     <Link to={item.name} className="tile">
       <img className="project-picture" src={`pictures/${item.imageLink}`} alt={item.name} />
-      <h4 className="project-title primary-font-color">{item.name}</h4>
+      <h3 className="project-title primary-font-color">{item.name}</h3>
     </Link>
   );
 }
